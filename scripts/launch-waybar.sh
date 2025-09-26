b4942aabefdff10dfb005c8d2f09f9bd0add6d5c
Saatvik Lochan <saatviklochan@gmail.com>
2025-09-26 10:56:11 -0500

    Add waybar autoupdate and change colour

---
+++ scripts/launch-waybar.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# from: @Hubro here https://github.com/Alexays/Waybar/issues/961#issuecomment-753533975
+
+CONFIG_FILES="$HOME/.config/waybar/config $HOME/.config/waybar/style.css"
+
+trap "killall waybar" EXIT
+
+while true; do
+    waybar &
+    inotifywait -e create,modify $CONFIG_FILES
+    killall waybar
+done
