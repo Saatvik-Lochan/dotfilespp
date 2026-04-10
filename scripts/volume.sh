fe98e13935d6e93f8948f8da3cf020c5ec20dc15
Saatvik Lochan <saatviklochan@gmail.com>
2026-04-10 16:09:54 -0500

    fixed volume to work on active sink

--- scripts/volume.sh
+++ scripts/volume.sh
@@ -1,14 +1,37 @@
 update_wob() {
-    awk '{ if (match($0, /\[(.*)%\]/, m)) { print m[1]; exit } }' \
-        > /tmp/wobpipe
+    pactl get-sink-volume "$1" | awk '{ if (match($0, /([0-9]+)%/, m)) { print m[1]; exit } }' > /tmp/wobpipe
+}
+
+current_sink() {
+    pactl list sink-inputs | awk '
+        /^Sink Input #[0-9]+/ { sink = ""; corked = "yes" }
+        /^\tSink: / { sink = $2 }
+        /^\tCorked: / {
+            corked = $2
+            if (sink != "" && corked == "no") {
+                print sink
+                exit
+            }
+        }
+    '
+}
+
+target_sink() {
+    current_sink || true
 }
 
 decrease() {
-    amixer -D pulse sset Master $1%- | update_wob
+    sink="$(target_sink)"
+    sink="${sink:-@DEFAULT_SINK@}"
+    pactl set-sink-volume "$sink" "-${1}%"
+    update_wob "$sink"
 }
 
 increase() {
-    amixer -D pulse sset Master $1%+ | update_wob
+    sink="$(target_sink)"
+    sink="${sink:-@DEFAULT_SINK@}"
+    pactl set-sink-volume "$sink" "+${1}%"
+    update_wob "$sink"
 }
 
 case $1 in 
